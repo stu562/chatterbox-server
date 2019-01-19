0f1c502200d74a934d33e3adca32b60f074413e1
Taylor Bantle <bantle.taylor@gmail.com>
2019-01-18 17:56:21 -0800

    Passed all server tests

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,6 +1,7 @@
 /* Import node's http module: */
 var http = require('http');
-
+// var url = require('url');
+var handleRequest = require('./request-handler.js');
 
 // Every server needs to listen on a port with a unique number. The
 // standard port for HTTP servers is port 80, but that port is
@@ -14,16 +15,22 @@ var port = 3000;
 // special address that always refers to localhost.
 var ip = '127.0.0.1';
 
-
+var routes = {
+    '/chatterbox/classes/messages': handleRequest
+}
 
 // We use node's http module to create a server.
-//
+
 // The function we pass to http.createServer will be used to handle all
 // incoming requests.
 //
 // After creating the server, we will tell it to listen on the given port and IP. */
-var server = http.createServer(handleRequest);
+var server = http.createServer(handleRequest.requestHandler);
+// var server = http.createServer(function(request, response) {
+//     var urlParts = url.parse(request.url);
+// });
 console.log('Listening on http://' + ip + ':' + port);
+
 server.listen(port, ip);
 
 // To start this server, run:
--- server/request-handler.js
+++ server/request-handler.js
@@ -11,14 +11,15 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+var object = {'results': []};
 
-var requestHandler = function(request, response) {
+var requestHandler = function (request, response) {
   // Request and Response come from node's http module.
-  //
+  // var urlParts = url.parse(request.url
   // They include information about both the incoming request, such as
   // headers and URL, and about the outgoing response, such as its status
   // and content.
-  //
+    
   // Documentation for both request and response can be found in the HTTP section at
   // http://nodejs.org/documentation/api/
 
@@ -28,22 +29,55 @@ var requestHandler = function(request, response) {
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
   // The outgoing status.
   var statusCode = 200;
-
   // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
-
   // Tell the client we are sending them plain text.
-  //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
   headers['Content-Type'] = 'text/plain';
+  
+  if (request.url === '/classes/messages'){
+    if (request.method === 'GET') {
+      response.writeHead(200);//code the server reads 
+    } 
+    if (request.method === 'POST') {
+      //changes status 200 get , 201 post
+      // response.writeHead(201);
+      var emptyString = '';
+      //method on the request object, pass first 
+      //if first argument on request.on , first callback data 
+      request.on('data', (data) => {
+        emptyString += data;
+        console.log(emptyString);
+        console.log(data)
+        //take results and push 
+        // object.results.push(JSON.parse(emptyString))// parse the string and store in the containe
+      })
+      request.on('end', function() {
+        object.results.push(JSON.parse(emptyString))
+        
+      })
+      
+      response.writeHead(201, headers);//write head changes the method of the object 
+      response.end(); //end transmission
+    }
+  } else {
+    response.writeHead(404);
+    response.end();
+  }
+  // request.on('end', function() {
+  //   console.log(data);
+  //   response.write('hi');
+  //   response.end();
+  // });
+  //parse
+  //push to results
 
   // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  // which includes the status and all headers
+  // response.writeHead(statusCode, headers);
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -52,8 +86,11 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+  response.end(JSON.stringify(object));
+  // we are sending back 
 };
+
+module.exports.requestHandler = requestHandler;
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
 // This code allows this server to talk to websites that
